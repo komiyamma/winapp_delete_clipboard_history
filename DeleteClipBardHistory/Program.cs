b73e5887f629dbcd62690c9cfc0cb38f19dbab18
komiyamma <komiyama0629@gmail.com>
2025-04-29 18:22:39 +0900

    クリップボード尾履歴ONの時に最後の履歴だけ削除する

---
+++ DeleteClipBardHistory/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DeleteClipBoardHistory
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            var history = Windows.ApplicationModel.DataTransfer.Clipboard.GetHistoryItemsAsync().GetResults();
+            if (history == null)
+            {
+                return 0;
+            }
+            int count = history.Items.Count;
+
+            if (count >= 1)
+            {
+                var contentView = history.Items[1];
+                Windows.ApplicationModel.DataTransfer.Clipboard.DeleteItemFromHistory(history.Items[0]);
+            }
+
+            if (count > 64)
+            {
+                count = 64;
+            }
+            return count;
+        }
+    }
+}
